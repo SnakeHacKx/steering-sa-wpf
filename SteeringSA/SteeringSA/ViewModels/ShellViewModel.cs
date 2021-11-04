6c66ee6cc9c5c64ccad2cef7baeb56b5b5fe9c35
SnakeHacKx <omar.medina@utp.ac.pa>
2021-11-04 00:09:31 -0500

    Iniciando la interfaz, creados hasta ahora muchos estilos, 3 views y las bases para comenzar bien el proyecto

---
+++ SteeringSA/SteeringSA/ViewModels/ShellViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SteeringSA.ViewModels
+{
+    class ShellViewModel : Conductor<object>
+    {
+        private WindowState _currentWindowState;
+
+        public WindowState CurrentWindowState
+        {
+            get { return _currentWindowState; }
+            set 
+            { 
+                _currentWindowState = value;
+                NotifyOfPropertyChange(() => CurrentWindowState);
+            }
+        }
+
+        public ShellViewModel()
+        {
+            CurrentWindowState = WindowState.Maximized;
+            ActivateItemAsync(new LoginViewModel());
+        }
+
+        public void ShutdownApp()
+        {
+            Application.Current.Shutdown();
+        }
+
+        public void MaximizeWindow()
+        {
+            if (CurrentWindowState == WindowState.Maximized)
+            {
+                CurrentWindowState = WindowState.Normal;
+            }
+            else if (CurrentWindowState == WindowState.Normal)
+            {
+                CurrentWindowState = WindowState.Maximized;
+            }
+        }
+
+        public void MinimizeWindow()
+        {
+            CurrentWindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ SteeringSA/SteeringSA/Views/ShellView.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SteeringSA.Views
+{
+    /// <summary>
+    /// Interaction logic for ShellView.xaml
+    /// </summary>
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            BrushConverter bc = new BrushConverter();
+            if (this.WindowState == WindowState.Normal)
+            {
+
+                MainWindowBorder.BorderThickness = new Thickness(2);
+                MainWindowBorder.BorderBrush = (Brush)bc.ConvertFrom("#4FCDF5");
+            }
+            else if (this.WindowState == WindowState.Maximized)
+            {
+                MainWindowBorder.BorderThickness = new Thickness(0);
+            }
+        }
+    }
+}
